9c4f05b09924063d01729c5ba6fa25309716ff23
David Clarke <david.j.d.clarke@gmail.com>
2023-01-21 15:43:30 -0500

    resolve test persistance issue

--- app.py
+++ app.py
@@ -3,19 +3,16 @@ import os
 import sys
 from load_dotenv import load_dotenv
 
-
-import streamlit as st
-
 root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root)
 load_dotenv(".env")
 
-from documents.streamlit import StreamlitManager
+from documents.streamlit import StreamlitRunner
 from documents.openai import OpenAIConnector
 
 
 if __name__ == "__main__":
     openai_connector = OpenAIConnector(os.environ.get("OPENAI_API_KEY"))
-    streamlit_manager = StreamlitManager(openai_connector)
+    streamlit_manager = StreamlitRunner(openai_connector)
 
     streamlit_manager.run()
