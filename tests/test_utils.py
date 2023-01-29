0d643304f89a6b578da0c44b953574d0bdb7b555
David Clarke <david.j.d.clarke@gmail.com>
2023-01-29 10:25:18 -0500

    update files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,14 +7,14 @@ root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root)
 load_dotenv(".env")
 
-from documents.utils import write_text_file, read_text_file, convert_pdf_to_txt
+from documents.utils import save_file, read_file, convert_pdf_to_txt
 
 
 def test_convert_pdf_to_txt_with_valid_file():
     pdf_file = "tests/resources/APPL_10-K-2022.pdf"
-    expected_output = read_text_file("tests/resources/outputs/APPL_10-K-2022.txt")
+    expected_output = read_file("tests/resources/outputs/APPL_10-K-2022.txt")
     output = convert_pdf_to_txt(pdf_file)
-    # write_text_file("tests/resources/outputs/APPL_10-K-2022.txt", output)
+    # save_file("tests/resources/outputs/APPL_10-K-2022.txt", output)
     assert output == expected_output
 
 
